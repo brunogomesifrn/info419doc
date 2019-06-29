f84c121c9097d12a753c4858559e0c30738dcfe0
RaissaMonteiro <rayssasilva691@gmail.com>
2019-06-29 11:36:50 -0300

    Atualizei

--- core/forms.py
+++ core/forms.py
@@ -4,19 +4,8 @@ from .models import cadastro
 
 class cadastroForm(ModelForm):
 	class Meta():
-<<<<<<< HEAD
-=======
-
 		model = cadastro
 		fields = ['usuario', 'senha', 'repetir_senha']
-
-
->>>>>>> 8092c1ecaab74a4b1b80a91626a62c6d23629a2c
-		model = Cursos
-		fields = ['usuario', 'senha', 'repetir_senha']
-
-		model = Tipo
-		fields = ['nome']
 
 class TipoForm(ModelForm):
 	class Meta():
@@ -26,11 +15,5 @@ class TipoForm(ModelForm):
 class DocForm(ModelForm):
 	class Meta():
 		model = Doc
-
-
 		fields = ['nome', 'data', 'arquivo', 'local']
 
-		fields = ['nome', 'data_inicio', 'local']
-
-
-		fields = ['nome', 'data', 'arquivo', 'local']
