95cf22b9918863bd5fea1a6d4a876f7697f29df3
RaissaMonteiro <rayssasilva691@gmail.com>
2019-06-29 12:46:12 -0300

    Atualizei

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class Tipo(models.Model):
 
 class Doc(models.Model):
 	nome = models.CharField('Nome', max_length=100)
-    data = models.DateTimeField('Data')
     arquivo = models.FileField('Arquivo', upload_to='upload')
-    local = models.CharField('Local Físico' ax_length=100
+    local = models.CharField('Local Físico' ax_length=100)
+    data = models.DateTimeField('Data')
 
 
