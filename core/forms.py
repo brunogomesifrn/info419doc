77c56a1a0148fc5680cfd83ccb2cfc51130ef025
RaissaMonteiro <rayssasilva691@gmail.com>
2019-06-29 10:10:41 -0300

    Atualizei

--- core/forms.py
+++ core/forms.py
@@ -3,17 +3,12 @@ from .models import Tipo, Doc
 
 class TipoForm(ModelForm):
 	class Meta():
-<<<<<<< HEAD
 		model = Cursos
 		fields = ['usuario', 'senha', 'repetir_senha']
-=======
 		model = Tipo
 		fields = ['nome']
-
-
 
 class DocForm(ModelForm):
 	class Meta():
 		model = Doc
 		fields = ['nome', 'data_inicio', 'local']
->>>>>>> 6b34178ff8d05853bb19bacd45977e68b18b41b9
--- core/models.py
+++ core/models.py
@@ -3,9 +3,6 @@ from django.db import models
 class Tipo(models.Model):
 	nome = models.CharField('Tipo', max_length=100)
 
-class Tipo_Cadastro(models.Model):
-	nome = models.CharField('Tipo', max_length=100)
-
 class Doc(models.Model):
 	nome = models.CharField('Nome', max_length=100)
     data_inicio = models.DateField('Data de Início', null=True)
