3f640aefc9db45e5e0340b33d5d7f3f69fe8c2d0
Projeito <clotildegomes1302@gmail.com>
2019-06-29 15:29:16 -0300

    atualizei

--- info419doc/urls.py
+++ info419doc/urls.py
@@ -53,9 +53,9 @@ urlpatterns = [
 
     #Autentificação
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(),
-    name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
