d7957b286f2d2e8789c310bb1236f38e61be56ba
RaissaMonteiro <rayssasilva691@gmail.com>
2019-06-29 10:36:34 -0300

    Atualizei

--- info419doc/settings.py
+++ info419doc/settings.py
@@ -38,15 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-<<<<<<< HEAD
-    <<<< HEAD
-   
-<<<
-=======
-    
->>>>>>> f023a75d5733e6a97f77a0e051d06139da60ce93
-=======
->>>>>>> fc9a3f830ec3ee0d5f9da7e0835f6a5a44e7906e
+
 ]
 
 MIDDLEWARE = [
